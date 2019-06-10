3ce51077e43b496eb4e516fb1e98a1867e7fc6b7
RM-RITIK <mehtaritik11@gmail.com>
2019-06-10 15:16:20 +0530

    15th commit

--- upload.php
+++ upload.php
@@ -83,8 +83,23 @@
         <option value="<?=$month;?>"><?=$month;?></option>
         <? endfor; ?>
         </select>
-        <button type="submit" id="submit" name="import"
-            class="btn-submit">Import</button>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Import</button>
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Are you sure you want to submit?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+        <button type="submit" id="submit" name="import" class="btn-submit btn-primary btn">Save changes</button>
+        </div>
+        </div>
+        </div>
+        </div>
         <br />
 
     </div>

79620dc36b560d3b54c236dfba983faea16c19c6
RM-RITIK <mehtaritik11@gmail.com>
2019-06-12 11:59:34 +0530

    25th commit

--- query4.php
+++ query4.php
@@ -7,6 +7,42 @@
  
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+
+      google.charts.load('current', {'packages':['corechart']});
+
+
+      google.charts.setOnLoadCallback(drawChart);
+
+
+      function drawChart() {
+
+ 
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Employee Name');
+        data.addColumn('number', 'Absent Days');
+        data.addRows([
+           <?php
+             $month = $_REQUEST['month'];
+             $x = strtotime($month);
+             $mo = date("m", $x);
+             $m = str_replace("0","", $mo);
+             $y = date("Y", $x);
+
+            ?>
+        ]);
+
+     
+        var options = {'title':'Absent Trends',
+                       'width':500,
+                       'height':300};
+
+        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
 
    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -21,13 +57,6 @@
   </head>
   <body>
     <?php include 'header.php'; ?>
-    <?php $month = $_REQUEST['month'];
-    $x = strtotime($month);
-    $mo = date("m", $x);
-    $m = str_replace("0","", $mo);
-    $y = date("Y", $x);
-     ?>
-
     <div class="form-inline form">
     <form class="form-group" action = "query4.php">
     <label for="inputPassword6">Month :</label>
@@ -35,6 +64,8 @@
     <button type="submit" class="btn btn-primary">Show</button>
     </form>
     </div>
+    <div id="chart_div"></div>
+
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
